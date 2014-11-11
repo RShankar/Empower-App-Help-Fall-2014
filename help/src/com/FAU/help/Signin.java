737cab04a589af1a0a7c4e4ab3a26a8eacee6430
amoubaye <amoubaye@fau.edu>
2014-11-11 17:13:08 -0500

    Project
    Started on the UI and the flow of the app

---
+++ help/src/com/FAU/help/Signin.java
@@ -0,0 +1,51 @@
+package com.FAU.help;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Signin extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View v) 
+	   {
+		 switch (v.getId())
+	    	{
+		 case R.id.signin:
+			 Intent i = new Intent(this, UserHome.class);
+	           startActivity(i);
+			 break;
+		 case R.id.createacc:
+			Intent j = new Intent(this,Signup.class);
+	        startActivity(j);
+			 break;
+	    	}
+	   }
+}
